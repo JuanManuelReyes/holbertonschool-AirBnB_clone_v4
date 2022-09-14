6bff17b392d605eedb570a6311c01c11ef6ab68d
Santiago Neira <santiagoneira52@gmail.com>
2022-09-14 21:42:35 +0000

    correct link to js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,14 +1,22 @@
 const idAmenity = [];
 
-console.log("holaaa");
 
-$(document).ready(function() {
-	console.log("dogchaw");
-	$("input[type=checkbox]").change(function(){
-		if ($(this).prop("checked")) {
-			idAmenity.push($(this).attr("id"))
-			console.log("aaaaaa", idAmenity)
-		}
-		console.log("ayuda")
-	})
-})
+// $(document).ready(function() {
+// 	$("input[type=checkbox]").change(function(){
+// 		if ($(this).prop("checked")) {
+// 			idAmenity.push($(this).attr("id"))
+// 			console.log("aaaaaa", idAmenity)
+// 		}
+// 	})
+// })
+
+$(document).ready(function () {
+	let checkedAmenities = {};
+	$("input[type=checkbox]").change(function () {
+	if ($(this).prop("checked")) {
+		idAmenity[$(this).data('id')] = $(this).data('name');
+	  } else {
+		delete idAmenity[$(this).data('id')];
+	  }
+	});
+  });
