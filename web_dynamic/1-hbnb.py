c83a38e93d3adfdd3d0bc5751c7d82dc329289f8
Santiago Neira <santiagoneira52@gmail.com>
2022-09-14 20:09:47 +0000

    fix task 1

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -36,12 +36,12 @@ def hbnb():
     places = sorted(places, key=lambda k: k.name)
     cache_id = uuid.uuid4()
 
-    return render_template('1-hbnb.html',
-                           states=st_ct,
-                           amenities=amenities,
-                           places=places,
-                           cache_id=cache_id)
-
+    # return render_template('1-hbnb.html',
+    #                        states=st_ct,
+    #                        amenities=amenities,
+    #                        places=places,
+    #                        cache_id=cache_id)
+	return "Hola"
 
 if __name__ == "__main__":
     """ Main Function """
