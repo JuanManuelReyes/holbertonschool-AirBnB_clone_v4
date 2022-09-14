a14bd0310f2688d9b5a1357276bd30b58fb36585
Santiago Neira <santiagoneira52@gmail.com>
2022-09-14 20:53:23 +0000

    try communicate with the browser

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,9 @@
 const idAmenity = [];
-$( document ).ready(function() {
+$(document).ready(function() {
 	$("input[type=checkbox]").change(function(){
 		if ($(this).prop("checked")) {
 			idAmenity.push($(this))
 			console.log("holaaa")
-		}
-		else{
-			// idAmenity.empty()
 		}
 	})
 })
