23eaf4ac73ab1364d0842e47a79ee4fffd670fd9
Santiago Neira <santiagoneira52@gmail.com>
2022-09-15 02:39:20 +0000

    delete element when the check is drawn

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,25 +1,15 @@
 const idAmenity = [];
 
-// $(document).ready(function() {
-// 	$("input[type=checkbox]").change(function(){
-// 		if ($(this).prop("checked")) {
-// 			idAmenity.push($(this).attr("id"))
-// 			console.log("aaaaaa", idAmenity)
-// 		}
-// 	})
-// })
-
 $(document).ready(function () {
 	let checkedAmenities = {};
 	$("input[type=checkbox]").change(function () {
 	if ($(this).prop("checked")) {
+		// AÑADIMOS EL ID DEL AMENITY CAMBIADO A LA LISTA
 		idAmenity.push($(this).data("id"))
-		console.log("aaaayuda", idAmenity)
+		// console.log("aaaayuda", idAmenity)
 	  } else {
+		// ELIMINAMOS EL ELEMENTO ESPECIFICO QUE FUE AÑADIDO POR POSITION
 		idAmenity.splice(idAmenity.indexOf($(this), 1));
-		console.log("chauu", idAmenity)
 	  }
 	});
   });
-
-  console.log(idAmenity);
