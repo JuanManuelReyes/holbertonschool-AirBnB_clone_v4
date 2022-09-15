25cc9e19c3bd7d7ee2fa04649ad125a530409880
Santiago Neira <santiagoneira52@gmail.com>
2022-09-15 03:18:11 +0000

    replace h4 with the content

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -4,18 +4,18 @@ $(document).ready(function () {
 	$("input[type=checkbox]").change(function () {
 	if ($(this).prop("checked")) {
 		// AÑADIMOS EL ID DEL AMENITY CAMBIADO A LA LISTA
-		idAmenity.push($(this).data("name"));
+		idAmenity[$(this).attr("data-id") = $(this).attr("data-name")];
 		// console.log("aaaayuda", idAmenity)
 	  } else {
 		// ELIMINAMOS EL ELEMENTO ESPECIFICO QUE FUE AÑADIDO POR POSITION
-		idAmenity.splice(idAmenity.indexOf($(this), 1));
+		delete idAmenity[$(this).attr('data-id')];
 	  }
 		//SI NUESTRA LISTA RESULTA ESTAR VACÍA REMPLAZAMOS CON ESPACIO VACÍO   
 	  if (idAmenity.length === 0)
-	  	$('div.amenities > h4').html("&nbsp;");
+	  	$('div.amenities h4').html("&nbsp;");
 		//SINO REMPLAZAMOS LOS H4 CON CADA ID DE AMENITY 
 	  else {
-		$("div.amenities > h4").text(Object.values(idAmenity).join(', '));
+		$("div.amenities h4").text(Object.values(idAmenity).join(', '));
 	  }
 	});
   });
